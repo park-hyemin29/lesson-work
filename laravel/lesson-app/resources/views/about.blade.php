c05135618726b765a33c68b661c1c7a9701dc73f
hyemin <hyemin@hyeminnoMacBook-Air.local>
2026-05-15 17:00:54 +0900

    feat: Laravel第2章の画面表示を追加

---
+++ laravel/lesson-app/resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+</head>
+<body>
+    <h1>Aboutページ</h1>
+    <p>Laravelの最初の画面です。</p>
+</body>
+</html>
